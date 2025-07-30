7073df6693fee4cfa45e541c296de8fb864b9167
Siddharth-Nmadev <namsiddharth19@gmail.com>
2025-07-30 16:52:16 +0530

    store chat in DB

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,15 +2,40 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { createSockerConnection } from "../utils/socket.js";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants.js";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-  const [message, setMessage] = useState([]);  // message ka array jisme msg ayenge
+  const [message, setMessage] = useState([]); // message ka array jisme msg ayenge
   const [newMessage, setNewMessage] = useState("");
   const user = useSelector((store) => store.user); // this give the loggedIn user
   const userId = user?._id;
 
-  useEffect(() => {    //id userId is not exist , not create connection
+  
+  const fetchChatMessage = async (req, res) => {
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+
+    const chatMessage = chat?.data?.message.map((msg) => {
+      const { senderId, text } = msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text,
+        timestamp: msg.createdAt,
+      };
+    });
+    setMessage(chatMessage);
+  };
+
+  useEffect(() => {
+    fetchChatMessage();
+  }, []);
+
+  useEffect(() => {
+    //id userId is not exist , not create connection
     if (!userId || !targetUserId) {
       console.error("Missing ID, cannot establish chat.");
       return; // Stop the function here
@@ -24,9 +49,9 @@ const Chat = () => {
       targetUserId,
     });
 
-    socket.on("messageReceived", ({ firstName, text }) => {
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
       console.log(firstName + " :  " + text);
-      setMessage((messages) => [...messages, { firstName, text }]);  // this set the message
+      setMessage((messages) => [...messages, { firstName, lastName, text }]); // this set the message
     });
 
     //when component unmount this will call
@@ -37,14 +62,28 @@ const Chat = () => {
 
   const sendMessage = () => {
     const socket = createSockerConnection();
+    const timestamp = new Date(); // Get the current time
+
     socket.emit("sendMessage", {
       firstName: user.firstName,
+      lastName: user.lastName,
       userId,
       targetUserId,
       text: newMessage,
+      timestamp: timestamp.toISOString(),
     });
     setNewMessage("");
   };
+  const formatTime = (timestamp) => {
+    if (!timestamp) return "";
+    return new Date(timestamp).toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+  };
+
+  
   return (
     <div className="w-full max-w-2xl mx-auto mt-8 bg-gradient-to-br from-blue-100 via-white to-indigo-100 shadow-lg rounded-2xl h-[70vh] flex flex-col border border-indigo-300">
       {/* Header */}
@@ -54,13 +93,21 @@ const Chat = () => {
 
       {/* Chat content */}
       <div className="flex-1 overflow-y-auto px-4 py-2 space-y-2 text-black">
-        {message.map((msg, index) => {   // ye loop message k pure array me chal rha h 
+        {message.map((msg, index) => {
+          // ye loop message k pure array me chal rha h
           return (
-            <div className="chat chat-start">
+            <div
+              className={
+                "chat " +
+                (user.firstName === msg.firstName ? "chat-end" : "chat-start")
+              }
+            >
               <div className="chat-image avatar"></div>
               <div className="chat-header">
-                {msg.firstName}
-                <time className="text-xs opacity-50">12:45</time>
+                {`${msg.firstName}  ${msg.lastName}`}
+                <time className="text-xs opacity-50 ml-2">
+                  {formatTime(msg.timestamp)}
+                </time>
               </div>
               <div className="chat-bubble">{msg.text}</div>
               <div className="chat-footer opacity-50">Delivered</div>
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,11 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
-export const createSockerConnection = ()=>{
-    return io(BASE_URL);   // this is for connect the backend 
-}
+export const createSockerConnection = () => {
+  // this is for connect the backend
+   if (location.hostname === "localhost") {
+    return io(BASE_URL); // this is for locathost
+  } else {
+    return io("/", { path: "/api/socket.io" });  // this is for production
+  }
+//return io(BASE_URL); 
+};
